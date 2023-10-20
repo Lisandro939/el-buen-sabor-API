a3f05bfa50619152dcf26f913f7e15c4b91b6b8a
Paula Ferreyra <95258267+P4ul4f@users.noreply.github.com>
2023-10-20 19:31:53 -0300

    Add files via upload
    pedido repo terminado

--- src/main/java/com/apiREST/API/Repositories/PedidoRepository.java
+++ src/main/java/com/apiREST/API/Repositories/PedidoRepository.java
@@ -1,7 +1,18 @@
 package com.apiREST.API.Repositories;
 
 import com.apiREST.API.Models.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+import java.util.List;
+
+public interface PedidoRepository extends BaseRepository<Pedido, Long> {
+
+    @Query(value = "SELECT * FROM pedido WHERE numero = :numero", nativeQuery = true)
+    List<Pedido> search(int numero);
+
+    @Query(value = "SELECT * FROM pedido WHERE numero = :numero", nativeQuery = true)
+    Page<Pedido> search(int numero, Pageable pageable);
 }
+
