b548d1d884ef60eae999a5d2ca2429be774222c8
Paula Ferreyra <95258267+P4ul4f@users.noreply.github.com>
2023-10-20 19:34:33 -0300

    Add files via upload
    pedido service impl terminado

---
+++ src/main/java/com/apiREST/API/Services/PedidoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.apiREST.API.Services;
+
+import com.apiREST.API.Models.Pedido;
+import com.apiREST.API.Repositories.BaseRepository;
+import com.apiREST.API.Repositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoServiceImpl extends BaseServiceImpl<Pedido, Long> implements PedidoService{
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public PedidoServiceImpl(BaseRepository<Pedido, Long> baseRepository, PedidoRepository pedidoRepository) {
+        super(baseRepository);
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    @Override
+    public List<Pedido> search(int numero) throws Exception {
+        try {
+            List<Pedido> entities = pedidoRepository.search(numero);
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Page<Pedido> search(int numero, Pageable pageable) throws Exception {
+        try {
+            Page<Pedido> entities = pedidoRepository.search(numero, pageable);
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+}
