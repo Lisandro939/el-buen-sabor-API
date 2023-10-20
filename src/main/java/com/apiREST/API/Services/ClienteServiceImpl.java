8777e86ea3fa02fb4dd504252f7d7093be5e1f25
Paula Ferreyra <95258267+P4ul4f@users.noreply.github.com>
2023-10-20 19:33:13 -0300

    Add files via upload
    cliente service impl terminado

---
+++ src/main/java/com/apiREST/API/Services/ClienteServiceImpl.java
@@ -0,0 +1,44 @@
+package com.apiREST.API.Services;
+
+import com.apiREST.API.Models.Cliente;
+import com.apiREST.API.Repositories.BaseRepository;
+import com.apiREST.API.Repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl extends BaseServiceImpl<Cliente, Long> implements ClienteService{
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public ClienteServiceImpl(BaseRepository<Cliente, Long> baseRepository, ClienteRepository clienteRepository) {
+        super(baseRepository);
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public List<Cliente> search(String filtro) throws Exception {
+        try {
+            List<Cliente> entities = clienteRepository.search(filtro);
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Page<Cliente> search(String filtro, Pageable pageable) throws Exception {
+        try {
+            Page<Cliente> entities = clienteRepository.search(filtro, pageable);
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+}
+
