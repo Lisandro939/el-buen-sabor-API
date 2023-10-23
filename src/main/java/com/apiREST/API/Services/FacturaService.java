ad8b5cfb736a3784315847a63349bd7e63fb2a21
FacuEspi04 <facunicoespinola123@gmail.com>
2023-10-23 17:45:25 -0300

    commit 2

---
+++ src/main/java/com/apiREST/API/Services/FacturaService.java
@@ -0,0 +1,16 @@
+package com.apiREST.API.Services;
+
+import com.apiREST.API.Models.Factura;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+
+public interface FacturaService extends BaseService<Factura, Long> {
+
+    List<Factura> search(String filtro) throws Exception;
+
+    Page<Factura> search(String filtro, Pageable pageable) throws Exception;
+
+}
