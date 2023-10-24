b6d9070162348302f1accac21f7b7f25b6649e05
nicocarcano <143526069+nicocarcano@users.noreply.github.com>
2023-10-24 13:41:57 -0300

    Add files via upload

--- src/main/java/com/apiREST/API/Repositories/RubroGeneralRepository.java
+++ src/main/java/com/apiREST/API/Repositories/RubroGeneralRepository.java
@@ -1,7 +1,20 @@
 package com.apiREST.API.Repositories;
 
+import com.apiREST.API.Models.RubroArticulo;
 import com.apiREST.API.Models.RubroGeneral;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RubroGeneralRepository extends JpaRepository<RubroGeneral, Long> {
+
+    @Query(value = "SELECT * FROM rubroGeneral WHERE filtro = :filtro", nativeQuery = true)
+    List<RubroGeneral> search(String filtro);
+
+    @Query(value = "SELECT * FROM rubroGeneral WHERE filtro = :filtro", nativeQuery = true)
+    Page<RubroGeneral> search(String filtro, Pageable pageable);
+
 }
