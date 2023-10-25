c4201153ec055f1985791722160c0c7734d94f37
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:56:46 -0300

    Update RubroGeneralRepository.java

--- src/main/java/com/apiREST/API/Repositories/RubroGeneralRepository.java
+++ src/main/java/com/apiREST/API/Repositories/RubroGeneralRepository.java
@@ -1,20 +1,21 @@
 package com.apiREST.API.Repositories;
 
-import com.apiREST.API.Models.RubroArticulo;
 import com.apiREST.API.Models.RubroGeneral;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface RubroGeneralRepository extends JpaRepository<RubroGeneral, Long> {
+@Repository
+public interface RubroGeneralRepository extends BaseRepository<RubroGeneral, Long> {
 
-    @Query(value = "SELECT * FROM rubroGeneral WHERE denominacion LIKE %?1%", nativeQuery = true)
+    @Query(value = "SELECT * FROM rubro_general WHERE denominacion LIKE %?1%", nativeQuery = true)
     List<RubroGeneral> search(String filtro);
 
-    @Query(value = "SELECT * FROM rubroGeneral WHERE denominacion LIKE %?1%", nativeQuery = true)
-    Page<RubroGeneral> search(String filtro, Pageable pageable);
+    @Query(value = "SELECT * FROM rubro_general WHERE denominacion LIKE %?1%", nativeQuery = true)
+    Page<RubroGeneral> searchPaged(String filtro, Pageable pageable);
+
 
 }
