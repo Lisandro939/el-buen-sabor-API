07a3bc59433b69927754de80a50e447212682282
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 20:03:17 -0300

    Update RubroArticuloService.java

--- src/main/java/com/apiREST/API/Services/RubroArticuloService.java
+++ src/main/java/com/apiREST/API/Services/RubroArticuloService.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface RubroArticuloService extends BaseService<RubroArticulo,Long>{
-    List<RubroArticulo> search (String filtro) throws Exception;
-    Page<RubroArticulo> search (String filtro, Pageable pageable) throws Exception;
+public interface RubroArticuloService extends BaseService<RubroArticulo, Long> {
+
+    List<RubroArticulo> search(String filtro) throws Exception;
+
+    Page<RubroArticulo> searchPaged(String filtro, Pageable pageable) throws Exception;
+
 }
