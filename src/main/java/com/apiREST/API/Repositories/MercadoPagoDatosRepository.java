f53bd82553c162f9e4f0876b7a0cb8aeaf9de315
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:55:21 -0300

    Update MercadoPagoDatosRepository.java

--- src/main/java/com/apiREST/API/Repositories/MercadoPagoDatosRepository.java
+++ src/main/java/com/apiREST/API/Repositories/MercadoPagoDatosRepository.java
@@ -1,17 +1,23 @@
 package com.apiREST.API.Repositories;
 
+import com.apiREST.API.Models.Domicilio;
+import com.apiREST.API.Models.Factura;
 import com.apiREST.API.Models.MercadoPagoDatos;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MercadoPagoDatosRepository extends BaseRepository<MercadoPagoDatos, Long> {
 
-    @Query(value = "SELECT * FROM mercadoPagoDatos WHERE identificador_pago LIKE %?1%", nativeQuery = true)
+    @Query(value = "SELECT * FROM mercado_pago_datos WHERE identificador_pago LIKE %?1%", nativeQuery = true)
     List<MercadoPagoDatos> search(String filtro);
 
-    @Query(value = "SELECT * FROM mercadoPagoDatos WHERE identificador_pago LIKE %?1%", nativeQuery = true)
-    Page<MercadoPagoDatos> search(String filtro, Pageable pageable);
+    @Query(value = "SELECT * FROM mercado_pago_datos WHERE identificador_pago LIKE %?1%", nativeQuery = true)
+    Page<MercadoPagoDatos> searchPaged(String filtro, Pageable pageable);
+
+
 }
