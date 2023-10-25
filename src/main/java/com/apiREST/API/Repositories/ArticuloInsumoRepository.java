f081dcd1881beae031903bf4edebe11ec8a479f8
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:52:37 -0300

    Update ArticuloInsumoRepository.java

--- src/main/java/com/apiREST/API/Repositories/ArticuloInsumoRepository.java
+++ src/main/java/com/apiREST/API/Repositories/ArticuloInsumoRepository.java
@@ -3,19 +3,15 @@ package com.apiREST.API.Repositories;
 import com.apiREST.API.Models.ArticuloInsumo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ArticuloInsumoRepository extends BaseRepository<ArticuloInsumo, Long> {
 
     @Query(value = "SELECT * FROM articulo_insumo WHERE denominacion LIKE %?1%", nativeQuery = true)
     List<ArticuloInsumo> search(String filtro);
 
     @Query(value = "SELECT * FROM articulo_insumo WHERE denominacion LIKE %?1%", nativeQuery = true)
-    Page<ArticuloInsumo> search(String filtro, Pageable pageable);
-
+    Page<ArticuloInsumo> searchPaged(String filtro, Pageable pageable);
 }
