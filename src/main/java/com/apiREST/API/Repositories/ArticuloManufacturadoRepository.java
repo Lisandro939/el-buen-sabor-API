1ff60de4c006697ebc7cd60e092e077a57174829
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:52:57 -0300

    Update ArticuloManufacturadoRepository.java

--- src/main/java/com/apiREST/API/Repositories/ArticuloManufacturadoRepository.java
+++ src/main/java/com/apiREST/API/Repositories/ArticuloManufacturadoRepository.java
@@ -1,19 +1,17 @@
 package com.apiREST.API.Repositories;
 
-import com.apiREST.API.Models.ArticuloInsumo;
 import com.apiREST.API.Models.ArticuloManufacturado;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ArticuloManufacturadoRepository extends JpaRepository<ArticuloManufacturado, Long> {
+public interface ArticuloManufacturadoRepository extends BaseRepository<ArticuloManufacturado, Long> {
 
     @Query(value = "SELECT * FROM articulo_manufacturado WHERE denominacion LIKE %?1%", nativeQuery = true)
     List<ArticuloManufacturado> search(String filtro);
 
     @Query(value = "SELECT * FROM articulo_manufacturado WHERE denominacion LIKE %?1%", nativeQuery = true)
-    Page<ArticuloManufacturado> search(String filtro, Pageable pageable);
+    Page<ArticuloManufacturado> searchPaged(String filtro, Pageable pageable);
 }
