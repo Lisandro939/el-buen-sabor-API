ff2984e92b73cfa0879df13df2b4065973a4a1ca
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:48:44 -0300

    Update FacturaController.java

--- src/main/java/com/apiREST/API/Controllers/FacturaController.java
+++ src/main/java/com/apiREST/API/Controllers/FacturaController.java
@@ -6,14 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/factura")
+@RequestMapping(path = "api/v1/facturas")
 public class FacturaController extends BaseControllerImpl<Factura, FacturaServiceImpl> {
 
     @GetMapping("/search")
-    public ResponseEntity<?> search(@RequestParam int filtro) {
+    public ResponseEntity<?> search(@RequestParam String filtro) {
         try {
             return ResponseEntity.status(200).body(servicio.search(filtro));
         } catch (Exception e) {
@@ -22,9 +21,9 @@ public class FacturaController extends BaseControllerImpl<Factura, FacturaServic
     }
 
     @GetMapping("/searchpaged")
-    public ResponseEntity<?> search(@RequestParam int filtro, Pageable pageable) {
+    public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
         try {
-            return ResponseEntity.status(200).body(servicio.search(filtro, pageable));
+            return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
         } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
