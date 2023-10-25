3e2a39165f5496d351ed624d65c4e53175d2e722
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:51:10 -0300

    Update RubroGeneralController.java

--- src/main/java/com/apiREST/API/Controllers/RubroGeneralController.java
+++ src/main/java/com/apiREST/API/Controllers/RubroGeneralController.java
@@ -1,16 +1,19 @@
 package com.apiREST.API.Controllers;
 
+import com.apiREST.API.Models.Pedido;
+import com.apiREST.API.Models.RubroArticulo;
 import com.apiREST.API.Models.RubroGeneral;
+import com.apiREST.API.Services.PedidoServiceImpl;
+import com.apiREST.API.Services.RubroArticuloServiceImpl;
 import com.apiREST.API.Services.RubroGeneralServiceImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/rubroGenerales")
-public class RubroGeneralController extends BaseControllerImpl<RubroGeneral, RubroGeneralServiceImpl>{
+@RequestMapping(path = "api/v1/rubrosGenerales")
+public class RubroGeneralController extends BaseControllerImpl<RubroGeneral, RubroGeneralServiceImpl> {
 
     @GetMapping("/search")
     public ResponseEntity<?> search(@RequestParam String filtro) {
@@ -22,12 +25,11 @@ public class RubroGeneralController extends BaseControllerImpl<RubroGeneral, Rub
     }
 
     @GetMapping("/searchpaged")
-    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable) {
+    public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
         try {
-            return ResponseEntity.status(200).body(servicio.search(filtro, pageable));
+            return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
         } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
     }
-
 }
