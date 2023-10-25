b46524871a7cd42ed2d1f30b6f9296fdccd3ac67
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:53:42 -0300

    Update ClienteRepository.java

--- src/main/java/com/apiREST/API/Repositories/ClienteRepository.java
+++ src/main/java/com/apiREST/API/Repositories/ClienteRepository.java
@@ -3,16 +3,19 @@ package com.apiREST.API.Repositories;
 import com.apiREST.API.Models.Cliente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ClienteRepository extends BaseRepository<Cliente, Long> {
 
-    @Query(value = "SELECT * FROM cliente WHERE nombre LIKE %?1%  OR apellido LIKE %?1%", nativeQuery = true)
+    @Query(value = "SELECT * FROM cliente WHERE nombre LIKE %?1%", nativeQuery = true)
     List<Cliente> search(String filtro);
 
-    @Query(value = "SELECT * FROM cliente WHERE nombre LIKE %?1% OR apellido LIKE %?1%", nativeQuery = true)
-    Page<Cliente> search(String filtro, Pageable pageable);
+    @Query(value = "SELECT * FROM cliente WHERE nombre LIKE %?1%", nativeQuery = true)
+    Page<Cliente> searchPaged(String filtro, Pageable pageable);
+
+
 }
