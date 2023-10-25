e926e6a17aac3eb8059d18b4b71bf25ab1be5c2b
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:58:43 -0300

    Update ArticuloManufacturadoService.java

--- src/main/java/com/apiREST/API/Services/ArticuloManufacturadoService.java
+++ src/main/java/com/apiREST/API/Services/ArticuloManufacturadoService.java
@@ -1,16 +1,16 @@
 package com.apiREST.API.Services;
 
+import com.apiREST.API.Models.ArticuloInsumo;
 import com.apiREST.API.Models.ArticuloManufacturado;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface ArticuloManufacturadoService extends BaseService<ArticuloManufacturado, Long>{
+public interface ArticuloManufacturadoService extends BaseService<ArticuloManufacturado, Long> {
 
     List<ArticuloManufacturado> search(String filtro) throws Exception;
 
-    Page<ArticuloManufacturado> search(String filtro, Pageable pageable) throws Exception;
+    Page<ArticuloManufacturado> searchPaged(String filtro, Pageable pageable) throws Exception;
 
 }
