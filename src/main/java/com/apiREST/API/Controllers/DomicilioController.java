67929acc5ad73a9fcc4e7180058a7280f676afea
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:47:35 -0300

    Update DomicilioController.java

--- src/main/java/com/apiREST/API/Controllers/DomicilioController.java
+++ src/main/java/com/apiREST/API/Controllers/DomicilioController.java
@@ -1,16 +1,17 @@
 package com.apiREST.API.Controllers;
 
 import com.apiREST.API.Models.Domicilio;
+import com.apiREST.API.Models.Pedido;
 import com.apiREST.API.Services.DomicilioServiceImpl;
+import com.apiREST.API.Services.PedidoServiceImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "/api/v1/domicilio")
-
-public class DomicilioController extends BaseControllerImpl<Domicilio, DomicilioServiceImpl>{
+@RequestMapping(path = "api/v1/domicilios")
+public class DomicilioController extends BaseControllerImpl<Domicilio, DomicilioServiceImpl> {
 
     @GetMapping("/search")
     public ResponseEntity<?> search(@RequestParam String filtro) {
@@ -22,12 +23,11 @@ public class DomicilioController extends BaseControllerImpl<Domicilio, Domicilio
     }
 
     @GetMapping("/searchpaged")
-    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable) {
+    public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
         try {
-            return ResponseEntity.status(200).body(servicio.search(filtro, pageable));
+            return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
         } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
     }
-
 }
