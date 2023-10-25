a8f81df2bec73043c40dbb8c9a68ecd2c234c2c6
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:50:08 -0300

    Update PedidoController.java

--- src/main/java/com/apiREST/API/Controllers/PedidoController.java
+++ src/main/java/com/apiREST/API/Controllers/PedidoController.java
@@ -1,15 +1,17 @@
 package com.apiREST.API.Controllers;
 
 import com.apiREST.API.Models.Pedido;
+import com.apiREST.API.Models.RubroArticulo;
 import com.apiREST.API.Services.PedidoServiceImpl;
+import com.apiREST.API.Services.RubroArticuloServiceImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/pedidos")
-public class PedidoController extends BaseControllerImpl<Pedido, PedidoServiceImpl> {
+@RequestMapping(path = "api/v1/rubrosArticulos")
+public class RubroArticuloController extends BaseControllerImpl<RubroArticulo, RubroArticuloServiceImpl> {
 
     @GetMapping("/search")
     public ResponseEntity<?> search(@RequestParam String filtro) {
