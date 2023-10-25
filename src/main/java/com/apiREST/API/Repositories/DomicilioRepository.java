3125293f89070a4cb8624977f0e2543854787deb
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:54:05 -0300

    Update DomicilioRepository.java

--- src/main/java/com/apiREST/API/Repositories/DomicilioRepository.java
+++ src/main/java/com/apiREST/API/Repositories/DomicilioRepository.java
@@ -1,19 +1,22 @@
 package com.apiREST.API.Repositories;
 
+import com.apiREST.API.Models.Cliente;
 import com.apiREST.API.Models.Domicilio;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
+@Repository
+public interface DomicilioRepository extends BaseRepository<Domicilio, Long> {
 
-    @Query(value = "SELECT * FROM domicilio WHERE numero LIKE %?1%", nativeQuery = true)
+    @Query(value = "SELECT * FROM domicilio WHERE calle LIKE %?1%", nativeQuery = true)
     List<Domicilio> search(String filtro);
 
-    @Query(value = "SELECT * FROM domicilio WHERE numero LIKE %?1%", nativeQuery = true)
-    Page<Domicilio> search(String filtro, Pageable pageable);
+    @Query(value = "SELECT * FROM domicilio WHERE calle LIKE %?1%", nativeQuery = true)
+    Page<Domicilio> searchPaged(String filtro, Pageable pageable);
+
 
 }
