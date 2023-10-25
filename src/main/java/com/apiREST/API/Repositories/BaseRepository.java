3353459d7b0c91fdb33c8504349759aa105cada3
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:53:15 -0300

    Update BaseRepository.java

--- src/main/java/com/apiREST/API/Repositories/BaseRepository.java
+++ src/main/java/com/apiREST/API/Repositories/BaseRepository.java
@@ -3,10 +3,9 @@ package com.apiREST.API.Repositories;
 import com.apiREST.API.Models.BaseEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 
-@Repository
+@NoRepositoryBean
 public interface BaseRepository <E extends BaseEntidad, ID extends Serializable> extends JpaRepository<E, ID> {
 }
