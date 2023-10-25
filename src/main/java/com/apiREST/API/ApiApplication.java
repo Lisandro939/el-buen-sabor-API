aa097110afacee9ec91f295bb181386e9344b8e4
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:43:14 -0300

    Update ApiApplication.java

--- src/main/java/com/apiREST/API/ApiApplication.java
+++ src/main/java/com/apiREST/API/ApiApplication.java
@@ -1,27 +1,15 @@
 package com.apiREST.API;
 
-import com.apiREST.API.Models.*;
-import com.apiREST.API.Repositories.ClienteRepository;
-import com.apiREST.API.Repositories.PedidoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-
-import java.sql.Date;
-import java.sql.Time;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "El buen sabor API", version = "1.0", description = "Documentación API El buen sabor"))
 public class ApiApplication {
-
-	@Autowired
-	PedidoRepository pedidoRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-		System.out.println("API REST corriendo en http://localhost:8080/");
+		System.out.println("API REST corriendo");
 	}
-
 }
