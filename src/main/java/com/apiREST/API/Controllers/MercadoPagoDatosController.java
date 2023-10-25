cf697c76e44d9187b82499e30d9087f744557008
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:49:08 -0300

    Update MercadoPagoDatosController.java

--- src/main/java/com/apiREST/API/Controllers/MercadoPagoDatosController.java
+++ src/main/java/com/apiREST/API/Controllers/MercadoPagoDatosController.java
@@ -1,8 +1,6 @@
 package com.apiREST.API.Controllers;
 
-
 import com.apiREST.API.Models.MercadoPagoDatos;
-import com.apiREST.API.Services.FacturaServiceImpl;
 import com.apiREST.API.Services.MercadoPagoDatosServiceImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/mercadoPago")
+@RequestMapping(path = "api/v1/mercadoPagoDatos")
 public class MercadoPagoDatosController extends BaseControllerImpl<MercadoPagoDatos, MercadoPagoDatosServiceImpl> {
 
     @GetMapping("/search")
@@ -23,9 +21,9 @@ public class MercadoPagoDatosController extends BaseControllerImpl<MercadoPagoDa
     }
 
     @GetMapping("/searchpaged")
-    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable) {
+    public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
         try {
-            return ResponseEntity.status(200).body(servicio.search(filtro, pageable));
+            return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
         } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
