08c4aaa0fe938624a2ab114e8f6eeecc4406d2c6
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:55:40 -0300

    Update PedidoRepository.java

--- src/main/java/com/apiREST/API/Repositories/PedidoRepository.java
+++ src/main/java/com/apiREST/API/Repositories/PedidoRepository.java
@@ -1,18 +1,22 @@
 package com.apiREST.API.Repositories;
 
+import com.apiREST.API.Models.Cliente;
 import com.apiREST.API.Models.Pedido;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PedidoRepository extends BaseRepository<Pedido, Long> {
 
-    @Query(value = "SELECT * FROM pedido WHERE numero = :numero", nativeQuery = true)
-    List<Pedido> search(int numero);
+    @Query(value = "SELECT * FROM pedido WHERE numero LIKE %?1%", nativeQuery = true)
+    List<Pedido> search(String filtro);
 
-    @Query(value = "SELECT * FROM pedido WHERE numero = :numero", nativeQuery = true)
-    Page<Pedido> search(int numero, Pageable pageable);
+    @Query(value = "SELECT * FROM pedido WHERE numero LIKE %?1%", nativeQuery = true)
+    Page<Pedido> searchPaged(String filtro, Pageable pageable);
+
+
 }
-
