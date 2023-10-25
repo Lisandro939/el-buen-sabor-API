cb834586bce107c7130cd4f0b9b323b3e6a91942
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-10-25 19:44:56 -0300

    Update ArticuloManufacturadoController.java

--- src/main/java/com/apiREST/API/Controllers/ArticuloManufacturadoController.java
+++ src/main/java/com/apiREST/API/Controllers/ArticuloManufacturadoController.java
@@ -1,6 +1,5 @@
 package com.apiREST.API.Controllers;
 
-
 import com.apiREST.API.Models.ArticuloManufacturado;
 import com.apiREST.API.Services.ArticuloManufacturadoServiceImpl;
 import org.springframework.data.domain.Pageable;
@@ -9,24 +8,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/articulosMan")
+@RequestMapping(path = "api/v1/articulosManufacturados")
 public class ArticuloManufacturadoController extends BaseControllerImpl<ArticuloManufacturado, ArticuloManufacturadoServiceImpl> {
 
     @GetMapping("/search")
-    public ResponseEntity<?> search(@RequestParam String filtro){
-        try{
+    public ResponseEntity<?> search(@RequestParam String filtro) {
+        try {
             return ResponseEntity.status(200).body(servicio.search(filtro));
-        } catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
     }
 
     @GetMapping("/searchpaged")
-    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable){
-        try{
-            return ResponseEntity.status(200).body(servicio.search(filtro, pageable));
-        } catch (Exception e){
-            return  ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
+    public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
+        try {
+            return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
     }
 }
