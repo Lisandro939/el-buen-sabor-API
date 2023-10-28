a35e1b1d28c7326d9c75a10808941a0f408c92a4
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-10-28 20:59:42 -0300

    fix

--- src/main/java/com/apiREST/API/Services/ArticuloInsumoServiceImpl.java
+++ src/main/java/com/apiREST/API/Services/ArticuloInsumoServiceImpl.java
@@ -40,8 +40,4 @@ public class ArticuloInsumoServiceImpl extends BaseServiceImpl<ArticuloInsumo,Lo
             throw new Exception(e.getMessage());
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 61101a2426169477ed1220dcfb2b350328a9b165
