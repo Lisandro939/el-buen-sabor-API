28d7f7f7ddb0d9a9f8c21c3b7d6eb32783169a23
Paula Ferreyra <95258267+P4ul4f@users.noreply.github.com>
2023-11-07 19:47:55 -0300

    Add files via upload

--- src/main/java/com/apiREST/API/Services/PedidoService.java
+++ src/main/java/com/apiREST/API/Services/PedidoService.java
@@ -1,15 +1,26 @@
 package com.apiREST.API.Services;
 
+import com.apiREST.API.Enums.EstadoPedido;
+import com.apiREST.API.Enums.TipoEnvio;
 import com.apiREST.API.Models.Pedido;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PedidoService extends BaseService<Pedido, Long> {
 
-    List<Pedido> search(String filtro) throws Exception;
+    List<Pedido> search(Date fechafiltro) throws Exception;
 
-    Page<Pedido> searchPaged(String filtro, Pageable pageable) throws Exception;
+    Page<Pedido> search(Date fechafiltro, Pageable pageable) throws Exception;
 
+
+    List<Pedido> searchByTipoEnvio(TipoEnvio tipoEnvio) throws Exception;
+
+    Page<Pedido> searchByTipoEnvio(TipoEnvio tipoEnvio, Pageable pageable) throws Exception;
+
+    List<Pedido> searchByEstadoPedido(EstadoPedido estado) throws Exception;
+
+    Page<Pedido> searchByEstadoPedido(EstadoPedido estado, Pageable pageable) throws Exception;
 }
