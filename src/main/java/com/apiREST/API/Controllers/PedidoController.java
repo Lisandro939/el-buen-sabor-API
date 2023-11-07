0e89ddc04c88b36e5ee281c8496b36e183e33068
Paula Ferreyra <95258267+P4ul4f@users.noreply.github.com>
2023-11-07 19:44:25 -0300

    Add files via upload

--- src/main/java/com/apiREST/API/Controllers/PedidoController.java
+++ src/main/java/com/apiREST/API/Controllers/PedidoController.java
@@ -1,10 +1,15 @@
 package com.apiREST.API.Controllers;
 
+import com.apiREST.API.Enums.EstadoPedido;
+import com.apiREST.API.Enums.TipoEnvio;
 import com.apiREST.API.Models.Pedido;
 import com.apiREST.API.Services.PedidoServiceImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -12,20 +17,69 @@ import org.springframework.web.bind.annotation.*;
 public class PedidoController extends BaseControllerImpl<Pedido, PedidoServiceImpl> {
 
     @GetMapping("/search")
-    public ResponseEntity<?> search(@RequestParam String filtro) {
+    public ResponseEntity<?> search(@RequestParam Date fechafiltro) {
         try {
-            return ResponseEntity.status(200).body(servicio.search(filtro));
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(fechafiltro));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
         }
     }
 
-    @GetMapping("/searchpaged")
-    public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
+    @GetMapping("/searchPaged")
+    public ResponseEntity<?> search(@RequestParam Date fechafiltro, Pageable pageable) {
         try {
-            return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(fechafiltro, pageable));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
+        }
+    }
+    // Tipo Envio
+    @GetMapping("/searchByTipoEnvio")
+    public ResponseEntity<?> searchByTipoEnvio(@RequestParam TipoEnvio tipoEnvio) {
+        try {
+//            TipoEnvio tipoEnvio = TipoEnvio.valueOf(tipoenvio);
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.searchByTipoEnvio(tipoEnvio));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Tipo Envio no válido: " + tipoEnvio + "\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
+        }
+    }
+
+    @GetMapping("/searchByTipoEnvioPaged")
+    public ResponseEntity<?> searchByTipoEnvio(@RequestParam TipoEnvio tipoEnvio, Pageable pageable) {
+        try {
+//            TipoEnvio tipoEnvio = TipoEnvio.valueOf(tipoEnvio);
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.searchByTipoEnvio(tipoEnvio, pageable));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Tipo Envio no válido: " + tipoEnvio + "\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
+        }
+    }
+
+    //Estado pedido
+    @GetMapping("/searchByEstado")
+    public ResponseEntity<?> searchByEstadoPedido(@RequestParam EstadoPedido estado) {
+        try {
+//            EstadoPedido estado = EstadoPedido.valueOf(Estado);
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.searchByEstadoPedido(estado));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Estado no válido: " + estado + "\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
+        }
+    }
+
+    @GetMapping("/searchByEstadoPaged")
+    public ResponseEntity<?> searchByEstadoPedido(@RequestParam EstadoPedido estado, Pageable pageable) {
+        try {
+//            EstadoPedido estado = EstadoPedido.valueOf(Estado);
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.searchByEstadoPedido(estado, pageable));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Estado no válido: " + estado + "\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
         }
     }
 }
