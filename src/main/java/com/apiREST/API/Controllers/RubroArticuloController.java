5fc5c334285ea3800cbb878f4d3971dff8db15cd
nicocarcano <143526069+nicocarcano@users.noreply.github.com>
2023-11-14 18:41:48 -0300

    Add files via upload

--- src/main/java/com/apiREST/API/Controllers/RubroArticuloController.java
+++ src/main/java/com/apiREST/API/Controllers/RubroArticuloController.java
@@ -1,10 +1,15 @@
 package com.apiREST.API.Controllers;
 
+import com.apiREST.API.DTOs.DTOABMRubroArticulo;
 import com.apiREST.API.Models.RubroArticulo;
 import com.apiREST.API.Services.RubroArticuloServiceImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -24,6 +29,27 @@ public class RubroArticuloController extends BaseControllerImpl<RubroArticulo, R
     public ResponseEntity<?> searchPaged(@RequestParam String filtro, Pageable pageable) {
         try {
             return ResponseEntity.status(200).body(servicio.searchPaged(filtro, pageable));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
+        }
+    }
+
+
+    @GetMapping("/searchABM")
+    public ResponseEntity<?> searchABM() {
+        try {
+            List<RubroArticulo> rubroArticulos = servicio.findAll();
+            List<DTOABMRubroArticulo> dtoabmRubroArticulos = new ArrayList<>();
+
+            for (RubroArticulo rubroArticulo: rubroArticulos){
+                DTOABMRubroArticulo dtoabmRubroArticulo = new DTOABMRubroArticulo();
+                dtoabmRubroArticulo.setRubro(rubroArticulo.getDenominacion());
+                dtoabmRubroArticulo.setRubroPadre(rubroArticulo.getDenominacion());
+                dtoabmRubroArticulo.setEstado(rubroArticulo.getEstado());
+                dtoabmRubroArticulos.add(dtoabmRubroArticulo);
+            }
+            return ResponseEntity.status(200).body(dtoabmRubroArticulos);
+
         } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\":\"Error, por favor intente más tarde.\"}");
         }
